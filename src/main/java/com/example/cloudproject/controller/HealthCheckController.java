6627050c8c9aa9a9cbb8f4b89e32eed74cfbec72
HAN-DABON <sinmint11@gmail.com>
2026-05-25 18:37:05 +0900

    lv5: 헬스체크 엔드포인트 작성

---
+++ src/main/java/com/example/cloudproject/controller/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.example.cloudproject.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+@Slf4j
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
